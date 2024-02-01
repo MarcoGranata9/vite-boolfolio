db2f4a73ebf458c89a80e5f0c74bb6b470d1d405
MarcoGranata9 <granatam9900@gmail.com>
2024-02-01 14:58:13 +0100

    add axios get projects api call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      apiUrl: 'http://127.0.0.1:8000',
+      projects: [],
+    }
+  },
+  created() {
+    axios.get(`${this.apiUrl}/api/projects`)
+      .then((resp) => {
+        console.log(resp);
+        this.projects = resp.data.results
+      })
+  },
+}
 </script>
 
 <template>
